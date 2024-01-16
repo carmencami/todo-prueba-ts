1c4fbd2f207eb5748cb1504d922337f4fc420441
carmencami <105524744+carmencami@users.noreply.github.com>
2024-01-16 20:58:00 +0100

    w

--- src/database.ts
+++ src/database.ts
@@ -9,8 +9,8 @@ import config from './config'
             // user: config.MONGO_USER,
             // pass: config.MONGO_PASS
         }
-        const db = await mongoose.connect('mongodb://localhost:27017/Mern', mongooseOptions);
-
+        const db = await mongoose.connect('mongodb://localhost:27017/mern-database', mongooseOptions);
+        
         console.log('Connected to MongoDB to : ' + db.connection.name)
     } catch (error) {
         console.error(error)
